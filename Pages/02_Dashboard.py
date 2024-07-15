500f237e36a1c8df9128ef64b99ac83886b3e121
RichieRichMoC <137105326+RichieRichMoC@users.noreply.github.com>
2024-07-15 00:39:28 +0000

    Update_for_presentation

--- Pages/02_Dashboard.py
+++ Pages/02_Dashboard.py
@@ -4,176 +4,255 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
- 
+from numerize.numerize import numerize
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+import matplotlib
+import warnings
+
 st.set_page_config(
     page_title='Dashboard',
     page_icon='📈',
     layout='wide'
 )
- 
+
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 # Check if the user is authenticated
 if not st.session_state.get("authentication_status"):
     st.info('Please log in to access the application from the homepage.')
 else:
  
-    st.title('Main Dashboard')
- 
     # Access data from session state
     data = st.session_state.get("data_key", None)
+  
+    dfz = data.drop(columns=['customerID'])
+      
+    table_feature = dfz.columns.tolist()
+      
     churn_rate = (data["Churn"].sum() / data.shape[0]) * 100
-    average_tenure = data['tenure'].mean()
-    average_monthly_charges = data['MonthlyCharges'].mean()
-    total_revenue = data['MonthlyCharges'].sum()
+    On_net_calls = data['MonthlyCharges'].sum()
+    average_MONTANT_charges = data['TotalCharges'].mean()
+    total_revenue = data['TotalCharges'].sum()
+    total_Users = data['customerID'].nunique()
 
-
-
-    st.markdown("<hr>", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center;'>KPI's</h1>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: center;'>KPI's</h5>", unsafe_allow_html=True)
     if data is not None:
         
            with st.container():
-                      
-                        cpp1, cpp2,cpp3, cpp4 = st.columns(4)
-                        with cpp1:
+                        
+                                 #3. columns
+                  total1,total2,total3,total4,total5 = st.columns(5,gap='small')
+                  with total1:
+
+                     st.markdown('<div class="column-style">Monthly Chrgs</div>', unsafe_allow_html=True)
+                     st.metric(label = '', value= f"{On_net_calls:,.0f}")
+
                      
-                         st.markdown(
-                f"""
+                     
+                  with total2:
+                     st.markdown('<div class="column-styl">Avg Income</div>', unsafe_allow_html=True)
                    
-                           <div style="background-color: #009900; border-radius: 10px; width: 80%; margin-top: 20px;" >
-                  
-                         <h4 style="margin-left: 30px">Churn Rate:
-                         </br>
-                          </br>
-                           
-                         {churn_rate:.2f}%.</h4>
-                             </div>
-                """,
-                unsafe_allow_html=True,
-            )
+                     st.metric(label='', value=f"{average_MONTANT_charges:,.0f}")
 
+                  with total3:
+                     st.markdown('<div class="column-sty">Total Charges</div>', unsafe_allow_html=True)
                    
-                        with cpp2:
-                         st.markdown(
-                f"""
-                   
-                           <div style="background-color: #ff9900; border-radius: 10px; width: 80%; margin-top: 20px;" >
+                     st.metric(label= '',value=f"{total_revenue:,.0f}")
+
+                  with total4:
+                     st.markdown('<div class="column-st">No_of_Users</div>', unsafe_allow_html=True)
+                    
+                     st.metric(label='',value=f"{total_Users:,.0f}")
+
+                  with total5:
+                     st.markdown('<div class="column-style">Churn Rate</div>', unsafe_allow_html=True)
                   
-                         <h4 style="margin-left: 30px">Avg Tenure: 
-                          </br>
-                          </br>
-                         {average_tenure:.2f} </h4>
-                             </div>
-                """,
-                unsafe_allow_html=True,
-            )
+                     st.metric(label='',value=numerize(churn_rate),help=f"""Total rating: {churn_rate}""")
+      
+                  st.markdown("""---""")
 
                         
-                        with cpp3:
-                         st.markdown(
-                f"""
-                   
-                           <div style="background-color: #3399ff; border-radius: 10px; width: 80%; margin-top: 20px;" >
-                 
-                          <h4 style="margin-left: 30px">Avg Mon Chgs:
-                            </br>
-                          </br>
-                            ${average_monthly_charges:.2f}</h4>
-                             </div>
-                """,
-                unsafe_allow_html=True,
-            )
-
-                        
-                        with cpp4:
-                         st.markdown(
-                f"""
-                   
-                           <div style="background-color: #ff6600; border-radius: 10px; width: 80%; margin-top: 20px;" >
-                  
-                        <h4 style="margin-left: 30px">Total Revenue: 
-                        
-                          </br>
-                          </br>
-                        ${total_revenue:.2f}</h4>
-                             </div>
-                """,
-                unsafe_allow_html=True,
-            )
-
-                                  
+                  unsafe_allow_html=True,
+               
     def main():
         
          
-            
+        st.markdown("<h5 style='text-align: center;'>EDA's</h5>", unsafe_allow_html=True) 
 
-        st.markdown("<hr>", unsafe_allow_html=True)           
-        st.markdown("<h1 style='text-align: center;'>EDA's</h1>", unsafe_allow_html=True)
+      
         with st.container():
-                    
-                    st.header('Univariate')
-                    co1, co2 = st.columns(2)
-                                   
-                    with co1:
-                        fig = px.histogram(data, x="Contract", color="Contract", barmode="group", height=400, width=500)
-                        fig.update_yaxes(title_text=None)
-                        st.plotly_chart(fig)
- 
-                    with co2:
-                        fig = px.box(data, x="tenure", height=400, width=500)
-                        st.plotly_chart(fig)
-        with st.container():
-                    cn1, cn2 = st.columns(2)
-                   
-                    with cn1:
-                        fig = px.histogram(data, x="TotalCharges", height=400, width=500)
-                        fig.update_yaxes(title_text=None)
-                        st.plotly_chart(fig)
- 
-                    with cn2:
-                        fig = px.histogram(data, x="MonthlyCharges", height=400, width=500)
-                        fig.update_yaxes(title_text=None)
-                        st.plotly_chart(fig)
-                       
-        with st.container():
-                    st.header('Bivariate')
-                    c1, c2 = st.columns(2)
-               
-                    with c1:
-                        senior_citizen_pie = px.pie(data, names='SeniorCitizen', title='Churn rate: Senior Vs Non-senior Citizens')
-                        st.plotly_chart(senior_citizen_pie, use_container_width=True)
- 
-                    with c2:
-                        fig = px.histogram(data, x='PaymentMethod', color='Churn', barmode='stack',
-                                color_discrete_map={'Yes': 'Firebrick', 'No': 'blue'},
-                                labels={'PaymentMethod': 'Payment Method', 'Churn': 'Churn'},
-                                title='Churn Patterns by Payment Method')
-                        fig.update_layout(xaxis_title='Payment Method', yaxis_title='Count', showlegend=True)
-                        st.plotly_chart(fig, use_container_width=True)        
-                   
-        with st.container():
-                    st.header('Multivariate')
-                    ca1, ca2 = st.columns(2)
-                   
- 
-                    with ca1:  
-                        data1 = data[["tenure", "MonthlyCharges", "TotalCharges", "Churn"]]
-                        fig = px.scatter_matrix(data1, dimensions=["tenure", "MonthlyCharges", "TotalCharges"],
-                                            color="Churn", symbol="Churn", title="Pairplot of Churn Features")
-                        st.plotly_chart(fig, use_container_width=True)
- 
-                    with ca2:
-                        correlation_matrix = data.corr(numeric_only=True)
-                        plt.figure(figsize=(10, 8))
-                        sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-                        plt.title("Correlation Matrix")
-                        st.pyplot(plt)
-                           
+                     
+                     data_df= data
+                     data_df.columns.tolist()
 
+                     cpp1, cpp2 = st.columns(2)
+                     with cpp1:
+                        st.title('Univariate Analysis')
+                     with cpp2:
+                           #selected_uni_feature = st.selectbox('Select Feature', data_df)
+                           selected_feature=st.selectbox('Select a Feature', options=table_feature, key='selected_model')
+
+                     co1, co2 = st.columns(2)
+                                    
+                     with co1:
+                           plt.figure(figsize=(8, 6))
+                           sns.histplot(data[selected_feature], kde=True)
+                           plt.xlabel(selected_feature)
+                           plt.title('Histogram')
+                           st.pyplot()
+
+                     with co2:
+                           plt.figure(figsize=(8, 6))
+                           sns.boxplot(data[selected_feature])
+                           plt.xlabel(selected_feature)
+                           plt.title('Boxplot')
+                           st.pyplot()
+
+        with st.container():
+                     st.title('Churn rate: For Per Selected Features')           
+
+                     cn1, cn2 = st.columns(2)
+                     
+                     with cn1:
+                           plt.figure(figsize=(8, 6))
+                           sns.histplot(data[selected_feature], kde=True)
+                           plt.xlabel(selected_feature)
+                           plt.title('Histogram')
+                           st.pyplot()
+
+                     with cn2:
+                           plt.figure(figsize=(8, 6))
+                           data[selected_feature].value_counts().plot.pie(autopct='%1.1f%%')
+                           plt.ylabel('')
+                           plt.title('Pie Chart')
+                           st.pyplot()
+
+        with st.container():
+                     cppo1, cppo2, cppo3= st.columns(3)
+                     with cppo1:
+                        st.title('Bivariate  Analysis')
+                     with cppo2:
+                           #selected_uni_feature = st.selectbox('Select Feature', data_df)
+                           selected_feature1=st.selectbox('Select a Feature', options=table_feature, key='selected_modeli')
+                     with cppo3:
+                           #selected_uni_feature = st.selectbox('Select Feature', data_df)
+                           selected_feature2=st.selectbox('Select a Feature', options=table_feature, key='selected_modela')
+                     c1, c2 = st.columns(2)
+                  
+                     with c1:
+                           plt.figure(figsize=(8, 6))
+                           sns.scatterplot(x=selected_feature1, y=selected_feature2, hue=selected_feature2, data=data)
+                           plt.xlabel(selected_feature1)
+                           plt.ylabel(selected_feature2)
+                           plt.title('Scatter Plot')
+                           plt.legend(title=selected_feature2)
+                           st.pyplot()
+
+                     with c2:
+                           plt.figure(figsize=(8, 6))
+                           pivot_data = data.pivot_table(index=selected_feature1, columns=selected_feature2, aggfunc='size')
+                           sns.heatmap(pivot_data, cmap='Blues', annot=True, fmt='g')
+                           plt.xlabel(selected_feature2)
+                           plt.ylabel(selected_feature1)
+                           plt.title('Heatmap')
+                           st.pyplot()
+
+        with st.container():
+                        df1 = data.drop(columns=['customerID','gender'])
+                     
+                        qppo1, qppo2, qppo3, qppo4, qppo5, qppo6 = st.columns(6)
+                        with qppo1:
+                           st.header('Multivariate')
+                        with qppo2:
+                           pass
+                        with qppo3:
+                           pass   
+                        with qppo4:
+                           z_variable = st.selectbox("Par1:", df1.columns)  
+                        with qppo5:
+                           x_variable = st.selectbox("Par2:", df1.columns)
+                        with qppo6:
+                           y_variable = st.selectbox("Par3:", df1.columns)    
+                     
+                     
+                        ca1, ca2 = st.columns(2)
+                        correlation_matrix = df1.corr(numeric_only=True)
+                     
+                        with ca1:  
+                            plt.figure(figsize=(10, 8))
+                            sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+                            plt.title("Correlation Matrix")
+                            st.pyplot()
+
+                        with ca2:
+                            fig = plt.figure(figsize=(10, 8))
+                            ax = fig.add_subplot(111, projection='3d')
+                            ax.scatter(df1[x_variable], df1[y_variable], df1[z_variable])
+                            ax.set_xlabel(x_variable)
+                            ax.set_ylabel(y_variable)
+                            ax.set_zlabel(z_variable)
+                            plt.title("3D Scatter Plot")
+                            st.pyplot()
+
+                   
+st.markdown(
+    """
+    <style>
+    .column-style {
+        background-color: #007bff;
+        color: white;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown(
+    """
+    <style>
+    .column-styl {
+        background-color: #FFA500;
+        color: white;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown(
+    """
+    <style>
+    .column-sty {
+        background-color: #28a745;
+        color: white;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown(
+    """
+    <style>
+    .column-st {
+        background-color: #ff0000;
+        color: white;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
           
  
-    if __name__ == '__main__':
+if __name__ == '__main__':
               main()
  
     
  
-          
